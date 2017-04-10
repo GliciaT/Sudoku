a9969d7f2d0070a6ee5118871666ffaabf7ae914
Glicia Tigre <gtrc@ecomp.poli.br>
2017-04-09 22:16:29 -0300

    Criada a classe Jogador

---
+++ Jogador.java
@@ -0,0 +1,20 @@
+class Jogador{
+	private String nome;
+	private int scoreRecord;
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public String getNome(){
+		return this.nome;
+	}
+
+	public void setScoreRecord(int scoreRecord){
+		this.scoreRecord = scoreRecord;
+	}
+
+	public int getScoreRecord(){
+		return this.scoreRecord;
+	}
+}
