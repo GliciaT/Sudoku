e41802a6e500cbf0c2d3935f327a51482c893246
Glicia Tigre <gtrc@ecomp.poli.br>
2017-04-09 22:35:05 -0300

    Acrescentado resolveTabuleiro e geraTabuleiro
    erro na linha 23 de return.

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -3,6 +3,9 @@ class Tabuleiro {
 	private int[][] gabarito;
 	private int[][] grid= new int[9][9];
 
+	/*Esse método recebe como entrada um
+	inteiro para a posição X, um inteiro para a posição Y e um inteiro para o valor (precisa
+	necessariamente ser um valor entre 1 e 9).*/
 	public boolean executaMovimento(int x, int y, int valor) {
 		if(valor<=9 && valor>=1) {
 			this.grid[x][y]=valor; 
@@ -11,6 +14,9 @@ class Tabuleiro {
 		else return false;
 	}
 	
+	/* Esse método deve percorrer todos os
+	valores do atributo grid e retornar false caso encontre algum valor == 0, caso contrário
+	retornar true.*/
 	public boolean isTabuleiroPreenchido() {
 		for(int i=0;i<9;i++) {
 			for(int j=0; i<9; i++) {
@@ -20,6 +26,16 @@ class Tabuleiro {
 				else return true;
 			}
 		}
-	}
+	} //está dando erro na linha 23, ainda n sei o que foi
 	
+	//implementar getters e setters
+	
+	public void resolveTabuleiro(){
+		//criar apenas a assinatura
+	}
+
+	public void geraTabuleiro(){
+		//criar apenas a assinatura
+		//vai receber um enum DificuldadePartida depois
+	}
 }
