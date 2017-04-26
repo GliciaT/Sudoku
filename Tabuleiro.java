efa03cba779c69430f1d652f3c84457be884458b
Glícia Tigre <gtrc@ecomp.poli.br>
2017-04-26 14:16:11 -0300

    Retirado comentarios do Tabuleiro.java

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -3,9 +3,6 @@ class Tabuleiro {
 	private int[][] gabarito;
 	private int[][] grid= new int[9][9];
 
-	/*Esse método recebe como entrada um
-	inteiro para a posição X, um inteiro para a posição Y e um inteiro para o valor (precisa
-	necessariamente ser um valor entre 1 e 9).*/
 	public boolean executaMovimento(int x, int y, int valor) {
 		if(valor<=9 && valor>=1) {
 			this.grid[x][y]=valor; 
@@ -14,9 +11,6 @@ class Tabuleiro {
 		else return false;
 	}
 	
-	/* Esse método deve percorrer todos os
-	valores do atributo grid e retornar false caso encontre algum valor == 0, caso contrário
-	retornar true.*/
 	public boolean isTabuleiroPreenchido() {
 		for(int i=0;i<this.grid.length;i++) {
 			for(int j=0; i<this.grid.length; i++) {
