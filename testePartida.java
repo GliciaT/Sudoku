51281ace56cc7b77edd720850ae0fe91a1c868bd
Glicia Tigre <gtrc@ecomp.poli.br>
2017-04-26 21:18:12 -0300

    Removido arquivos não usados

--- testePartida.java
+++
@@ -1,7 +0,0 @@
-public class testePartida {
-    public static void main(String[] args){
-        Partida jogo1 = new Partida("Amelia");
-        jogo1.escolherDificuldade("FACIL");
-        
-    }
-}
